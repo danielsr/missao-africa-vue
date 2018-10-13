6451d5bd486939b97cc8044294a3bbe5272cb959
Daniel Rodrigues <danielsr@gmail.com>
2018-10-13 15:49:26 -0300

    Login page style

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,7 +1,8 @@
 <template>
   <v-form @submit="onSubmit">
-    <v-modal title="Login">
-      <div class="container grid">
+    <div class="login-background">
+      <img src="https://www.missaoafrica.org.br/wp-content/uploads/2016/12/logo-e1481906450131.png">
+      <div class="login grid">
         <v-input
           name="email"
           label="E-mail"
@@ -19,12 +20,11 @@
           v-validate="'required'"
           :error="errors.first('pwd')"
         />
+        <div class="col-12">
+          <v-button text="Login" color="primary" :disabled="isInvalid"/>
+        </div>
       </div>
-      <template slot="footer">
-        <v-button text="Login" color="primary" :disabled="isInvalid"/>
-        <span v-if="msg">{{msg}}</span>
-      </template>
-    </v-modal>
+    </div>
   </v-form>
 </template>
 
@@ -63,3 +63,28 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+@import '@/style/vars.scss';
+
+.login-background {
+  position: absolute;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: $color-gray-dark;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.login {
+  background-color: $color-white;
+  padding: 20px;
+  width: 300px;
+  border-radius: 5px;
+  margin-top: 20px;
+}
+</style>
